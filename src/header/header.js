ef665a932392f35fea46f148c49137a68b9cea70
dariom25 <dario@maciey.de>
2023-10-12 20:58:36 +0200

    Understand webpack slowly

---
+++ src/header/header.js
@@ -0,0 +1,7 @@
+export function component() {
+    const element = document.createElement('div');
+ 
+    element.textContent = "Test";
+  
+    return element;
+  }
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./header/header-style.css";
+import "./body/body-style.css"
+import { component } from "./header/header";
