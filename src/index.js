0dbf34fa40c39975d881c33a22cdf3a5d3a14329
dariom25 <dario@maciey.de>
2023-10-13 21:36:10 +0200

    Load the header correctly

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import "./body/body-style.css";
 import "./sidebar/sidebar-style.css";
 import "./main-content/main-content-style.css";
 import "./footer/footer-style.css"
-import { component } from "./header/header";
+import { header } from "./header/header.js";
+
+const content = document.querySelector("#content");
+content.appendChild(header());
