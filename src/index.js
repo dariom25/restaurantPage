5ff9163d0b42922dda9acf810d3dde9a020c2a46
dariom25 <dario@maciey.de>
2023-10-13 22:01:34 +0200

    Update indext

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./sidebar/sidebar-style.css";
 import "./main-content/main-content-style.css";
 import "./footer/footer-style.css"
 import { header } from "./header/header.js";
+import { sidebar } from "./sidebar/sidebar.js";
 
 const content = document.querySelector("#content");
 content.appendChild(header());
+content.appendChild(sidebar());
