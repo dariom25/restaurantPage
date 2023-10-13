0ef12dcee33ac30db3ea4bd86950c07dfdceb351
dariom25 <dario@maciey.de>
2023-10-13 20:50:31 +0200

    Manage imports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./header/header-style.css";
-import "./body/body-style.css"
+import "./body/body-style.css";
+import "./sidebar/sidebar-style.css";
+import "./main-content/main-content-style.css";
+import "./footer/footer-style.css"
 import { component } from "./header/header";
