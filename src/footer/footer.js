7c7dc14c0cec76e6a04b7d5904b2bbe38f15a76a
dariom25 <dario@maciey.de>
2023-10-13 22:05:37 +0200

    Add footer.js and create a footer

---
+++ src/footer/footer.js
@@ -0,0 +1,7 @@
+export function footer() {
+    const footerDiv = document.createElement("div");
+    footerDiv.textContent = "Footer";
+    footerDiv.setAttribute("id", "footer");
+
+    return footerDiv
+}
