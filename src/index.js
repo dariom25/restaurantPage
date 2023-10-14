cf5382e9e4c858fef1cd185360b0241738f69d9d
dariom25 <dario@maciey.de>
2023-10-14 13:05:16 +0200

    Manage imports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import "./header/header-style.css";
 import "./body/body-style.css";
 import "./sidebar/sidebar-style.css";
 import "./main-content/main-content-style.css";
-import "./footer/footer-style.css"
+import "./footer/footer-style.css";
+import "./main-content/dishes/dishes-style.css";
 import { header } from "./header/header.js";
 import { sidebar } from "./sidebar/sidebar.js";
+import { footer } from "./footer/footer";
+
 
 const content = document.querySelector("#content");
 content.appendChild(header());
 content.appendChild(sidebar());
+content.appendChild(footer());
