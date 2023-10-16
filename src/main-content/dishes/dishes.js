1c511e1b0251f67c0af53ee4e8126723653df96c
dariom25 <dario@maciey.de>
2023-10-16 21:53:33 +0200

    Fix bugs and correct imports

--- src/main-content/dishes/dishes.js
+++ src/main-content/dishes/dishes.js
@@ -1,9 +1,11 @@
+import image from "./img/ramen.png"
+
 function dish(title, path, price, description) {
     const dishContainer = document.createElement("div");
     dishContainer.setAttribute("class", "dish");
 
     const titleDiv = document.createElement("div");
-    titleDiv.setAttribute("title");
+    titleDiv.setAttribute("class", "title");
     titleDiv.textContent = title;
     dishContainer.appendChild(titleDiv);
 
@@ -30,3 +32,30 @@ function dish(title, path, price, description) {
 
     return dishContainer
 }
+
+export function displayDishes() {
+    const titles = ["Asteroid Fried Rice", "Galactic Gnocchi", "Universal Umami Ramen"];
+    const paths = [
+        image,
+        image,
+        image,
+        image
+    ];
+    const prices = ["19.99$$", "19.99$$", "19.99$$", "19.99$$"];
+    const descriptions = [
+        "Fried rice with a variety of vegetables, shrimp, and chicken, seasoned with soy sauce, ginger and garlic. Veggie options available with tofu.",
+        "Gnochchi in a creamy tomato sauce topped with grilled shrimp and spinach.",
+        "A Japanese noodle soup with a variety of vegetables, pork and a savory broth.",
+        "A black bean and quinoa burger topped with avocado, sprouts and a sweet and sour, served on a toasted bun with a side of fries."
+    ];
+    const mainContent = document.createElement("div");
+    mainContent.setAttribute("id", "main-content");
+
+    for (let i = 0; i < titles.length; i++ ) {
+        const element = dish(titles[i], paths[i], prices[i], descriptions[i]);
+        mainContent.appendChild(element);
+        console.log("Test")
+    }
+    
+    return mainContent;
+}   
