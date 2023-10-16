ec8ae750c5ec985bb22237052ea6c642db0905ab
dariom25 <dario@maciey.de>
2023-10-16 21:58:16 +0200

    Add function to display the dished

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import "./main-content/dishes/dishes-style.css";
 import { header } from "./header/header.js";
 import { sidebar } from "./sidebar/sidebar.js";
 import { footer } from "./footer/footer";
-
+import { displayDishes } from "./main-content/dishes/dishes.js";
+import Icon from "./main-content/dishes/img/rice.png"
 
 const content = document.querySelector("#content");
 content.appendChild(header());
 content.appendChild(sidebar());
 content.appendChild(footer());
+content.appendChild(displayDishes());
