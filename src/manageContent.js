250f83bd1f600854983398b37eb77f6214382c8c
dariom25 <dario@maciey.de>
2023-10-19 20:31:50 +0200

    Create func load the home content

--- src/manageContent.js
+++ src/manageContent.js
@@ -1,7 +1,16 @@
-export function removeContent() {
+import { displayDishes } from "./main-content/dishes/dishes.js";
+import { displayContactDetails } from "./main-content/contact/contact.js";
+import { createHomeContent } from "./main-content/home/home.js";
+
+function removeContent() {
     const mainContentDiv = document.querySelector("#main-content");
     for (let i = 0; i < mainContentDiv.childNodes.length; i++) {
         mainContentDiv.removeChild(mainContentDiv.childNodes[i]);
     }
 }
 
+export function loadHomeContent() {
+    const mainContentDiv = document.querySelector("#main-content");
+    removeContent();
+    createHomeContent();
+}
