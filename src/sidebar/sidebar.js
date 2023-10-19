1e8a9b1198c8c8cc197cc5c62187647bc2ce9204
dariom25 <dario@maciey.de>
2023-10-19 20:50:40 +0200

    Change the func sidebar so that another class is added

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,7 +1,7 @@
-function sidebarTab(description) {
+function sidebarTab(description, className) {
     const element = document.createElement("button");
     element.textContent = description;
-    element.setAttribute("class", "tab");
+    element.classList.add("tab", className);
 
     return element
 }
@@ -10,11 +10,12 @@ export function sidebar() {
     const sidebarDiv = document.createElement("div");
     sidebarDiv.setAttribute("id", "sidebar")
     const tabs = ["Home", "Menu", "Contact"];
+    const classNames = ["home-button", "menu-button", "contact-button"];
 
-    
-    tabs.forEach((tab) => {
-        const element = sidebarTab(tab);
+    for (let i = 0; i < tabs.length; i++) {
+        const element = sidebarTab(tabs[i], classNames[i]);
         sidebarDiv.appendChild(element);
-    })
+    }
+
     return sidebarDiv
 }
