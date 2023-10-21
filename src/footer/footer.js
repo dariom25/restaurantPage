dc477d2737207fcc7b5c37f13a944b938a0ea00d
dariom25 <dario@maciey.de>
2023-10-21 18:47:00 +0200

    Add url for background image

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,6 +1,6 @@
 export function footer() {
     const footerDiv = document.createElement("div");
-    footerDiv.textContent = "Footer";
+    footerDiv.innerHTML = 'Photo by <a href="https://unsplash.com/@weirick?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jake Weirick</a> on <a href="https://unsplash.com/photos/the-night-sky-is-filled-with-stars-Q_RBVFFXR_g?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>';
     footerDiv.setAttribute("id", "footer");
 
     return footerDiv
